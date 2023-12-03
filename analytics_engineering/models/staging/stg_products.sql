45ba0c4852b4a199cadc7db7a3bc78235ffc47fc
root <root@LAPTOP-BJL479CR>
2023-12-03 06:12:35 +0100

    Removed ; issues from supplier_ids

--- analytics_engineering/models/staging/stg_products.sql
+++ analytics_engineering/models/staging/stg_products.sql
@@ -1,6 +1,21 @@
 with source as (
-
-    select * from {{ source('northwind', 'products') }}
+    select
+        CAST(supplier_ids AS integer) AS supplier_id,
+        id,
+        product_code,
+        product_name,
+        description,
+        standard_cost,
+        list_price,
+        reorder_level,
+        target_level,
+        quantity_per_unit,
+        discontinued,
+        minimum_reorder_quantity,
+        category,
+        attachments,
+    from {{ source('northwind', 'products') }}
+    where supplier_ids not like '%;%'
 )
 select
     *,
